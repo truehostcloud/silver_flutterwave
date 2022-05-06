6db7fabd0b21e463c246c5bf93f184d75a3cace3
idah-github <72487433+idah-github@users.noreply.github.com>
2022-05-06 10:37:54 +0300

    Revert "Revert "Feat/stripe integration""

--- apps.py
+++ apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class SilverFlutterwaveConfig(AppConfig):
     name = "silver_flutterwave"
+
+# https://github.com/truehostcloud/silver_flutterwave/tree/feat/stripe-integration
+#
