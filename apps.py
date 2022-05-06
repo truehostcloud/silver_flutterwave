ea4c19bc65f1413b5fc0104ec3e0afafaf0e21b6
William Mwai <willymwaid92@gmail.com>
2022-05-06 10:28:46 +0300

    Revert "Feat/stripe integration"

--- apps.py
+++ apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class SilverFlutterwaveConfig(AppConfig):
     name = "silver_flutterwave"
-
-# https://github.com/truehostcloud/silver_flutterwave/tree/feat/stripe-integration
-#
