3fd88d282c93df005820f19324b7c1ab217219b6
Your Name <you@example.com>
2022-05-10 08:29:38 +0300

    Stipe payment package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+long_description =(here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='silver_flutterwave',
+    version='2.0',
+    description='Stripe pay',
+    long_description='Stripe Payment in Olitt',
+    url='',
+    author='Idah',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+
+)
---
+++ src/silver/views.py
@@ -0,0 +1,36 @@
+from silver.payment_processors import get_instance
+from silver.payment_processors.views import GenericTransactionView
+from django.conf import settings
+import stripe
+from money.money import Money
+from money.currency import Currency
+
+
+class FlutterWaveTransactionView(GenericTransactionView):
+    @staticmethod
+    def get_stripe_client_secret(transaction):
+        """Get the Stripe client secret for the transaction."""
+        _ = "https://api.stripe.com/v1/payment_intents"
+        amount = transaction.invoice.total
+        currency = transaction.invoice.currency
+        smallest_amount_unit = Money(amount, getattr(Currency, currency)).sub_units
+        payload = {
+            "amount": smallest_amount_unit,
+            "currency": currency,
+            "automatic_payment_methods[enabled]": True,
+        }
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        intent_response = stripe.PaymentIntent.create(**payload)
+        return intent_response.get("client_secret")
+
+    def get_context_data(self):
+        context_data = super(FlutterWaveTransactionView, self).get_context_data()
+        payment_processor = get_instance(self.transaction.payment_processor)
+        context_data["client_token"] = payment_processor.client_token(
+            self.transaction.customer
+        )
+        context_data["is_recurring"] = payment_processor.is_payment_method_recurring(
+            self.transaction.payment_method
+        )
+        context_data["client_secret"] = self.get_stripe_client_secret(self.transaction)
+        return context_data
