3ed365ccd240e52cc074fe153f44fba32cac5c6a
Your Name <you@example.com>
2022-05-20 13:37:35 +0300

    Stripe pay

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="silver_flutterwave",
-    version="2.0.3",
+    version="2.0.4",
     description="Stripe pay",
     long_description="Stripe Payment in Olitt",
     url="",
     author="Idah",
+    package_data = {
+    'static': ['*'],
+    'Potato': ['*.txt']
+}
     # package_dir={"": "src"},
     # packages=find_packages(where="src"),
     packages=["silver_flutterwave"],
