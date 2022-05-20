75fadcd6c0898e79e4ec32a4d3def70b0a783ba3
Your Name <you@example.com>
2022-05-20 11:13:30 +0300

    Stripe

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="silver_flutterwave",
-    version="2.0.2",
+    version="2.0.3",
     description="Stripe pay",
     long_description="Stripe Payment in Olitt",
     url="",
